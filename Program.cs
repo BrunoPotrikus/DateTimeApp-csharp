ff4a94f9f72f0bb61437bdffc54d9c09851be3b6
BrunoPotrikus <brunopotrikus@gmail.com>
2022-12-23 18:36:09 -0300

    Primeiro commit, com uso de DateTime.Now

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        var data = new DateTime();
+        var dataNow = DateTime.Now;
+        Console.WriteLine(data);
+        Console.WriteLine(dataNow);
+    }
+}
